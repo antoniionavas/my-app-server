356942432709751609939cfe38f7b62b94190bd3
Antonio Navas <antonioluis_7_@hotmail.com>
2023-09-06 20:39:39 +0200

    "otro dia mas"

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
+const Band = require("../models/Band.model")
 const isAuthenticated = require("../middlewares/isAuthenticated");
 const uploader = require("../middlewares/cloudinary.js");
 
@@ -76,6 +77,44 @@ router.put("/update", isAuthenticated, uploader.single("profileImg"), async (req
        next(error)
     }
 })
+
+//POST /user/:bandId/fav => Añadimos a las bandas favoritas de la base de datos 
+router.post("/:bandId/fav", isAuthenticated, async (req, res, next) => {
+    try {
+      const oneBand = await Band.findById(req.params.bandId);
+      const user = await User.findById(req.payload._id);
+  
+      if (user.bandFav.includes(oneBand)) {
+        await User.findByIdAndUpdate(req.payload._id, {
+          $pull: { bandFav: oneBand },
+        });
+      } else {
+        await User.findByIdAndUpdate(req.payload._id, {
+          $addToSet: { bandFav: oneBand },
+        });
+      }
+      console.log("la banda",oneBand)
+      console.log("user", user)
+      res.json("La banda se ha añadido a las bandas favs")
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  //POST /user/:bandId/fav => Eliminamos la banda de favoritas de la base de datos los lugares
+  router.post("/:bandId/delete", isAuthenticated, async (req, res, next) => {
+    try {
+      const oneBand = await Band.findById(req.params.bandId);
+      const deleteBandFav = await User.findByIdAndUpdate(req.payload._id, {
+        $pull: { bandFav: req.params.bandId },
+      });
+      console.log(oneBand)
+      console.log(deleteBandFav)
+      res.json("La banda se ha eliminado de las bandas favs")
+    } catch (error) {
+      next(error);
+    }
+  });
 
 
 
